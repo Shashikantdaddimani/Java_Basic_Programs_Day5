bc5581c0c59853c4c88271756663183d95936fd8
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-10-01 11:01:05 +0530

    Adding 2D Array proram

---
+++ src/com/javabasics/day5/TwoDArray.java
@@ -0,0 +1,33 @@
+package com.javabasics.day5;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size : ");
+		int size = sc.nextInt();
+		int arr[][]=new int[size][size];
+		System.out.println("Enter row for the  array : ");
+		int row = sc.nextInt();
+		System.out.println("Enter column for the  array : ");
+		int col = sc.nextInt();
+		
+		System.out.println("Enter "+ (row *col) +" Array Elements: ");
+			for(int i=0;i<row;i++) {
+				for(int j=0;j<col;j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+		System.out.println("The Array Element:\n");
+			for(int i=0;i<row;i++) {
+				for(int j=0;j<col;j++) {
+					System.out.print(arr[i][j]+" ");
+				}
+				System.out.println();
+			}
+	}
+
+}
